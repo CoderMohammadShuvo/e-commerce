cfed21a781f12b00612ae49382eb67a35358f322
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-29 23:20:57 +0600

    footer completed

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,16 @@ import { GrYoutube } from "react-icons/gr";
 import { IoIosArrowDown } from "react-icons/io";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="min-h-screen ">
+      <main className="">
         <Navbar />
         <Banner />
         <ProductSet1 />
         <NewArrival />
-      </main>
-      <footer className="w-full bg-[#246400] text-white p-8">
-          <div className="w-1/3 mx-auto text-center flex flex-col justify-center gap-4">
+        <footer className="w-full bg-[#246400] text-white p-12 grid grid-cols-1 gap-10">
+          <div className="w-1/4 mx-auto text-center flex flex-col justify-center gap-4">
             <p className="text-2xl font-medium ">Subscribe to our Newslater</p>
-            <div className="cursor-pointer p-2 border-2 border-white flex justify-between items-center">
+            <div className="cursor-pointer p-2 border border-white flex justify-between items-center">
               <p>Email</p>
               <FaArrowRight />
             </div>
@@ -47,15 +46,17 @@ export default function Home() {
              <GrYoutube />
           </div>
           {/* LANGUAGE---- */}
-          <div>
+          <div className="w-5/6 mx-auto">
             <p>Language</p>
-            <div className="cursor-pointer p-2 border-2 border-white flex justify-between items-center">
+            <div className="cursor-pointer p-2 border border-white flex justify-between items-center w-[180px] my-2">
               <p>English</p>
               <IoIosArrowDown />
             </div>
             <p>© 2025, Captain Fashion Bd Powered by Shopify</p>
           </div>
       </footer>
+      </main>
+      
     </div>
   );
 }
