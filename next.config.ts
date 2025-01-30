1c59fae6228065b3769dcce5bc66e0f3650cbb88
CoderMohammadShuvo <mohammad1.sunny1@gmail.com>
2025-01-30 13:34:51 +0600

    latest

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.ignoreWarnings = [/Module not found/]; // Ignores missing modules
+    return config;
+  },
 };
 
 export default nextConfig;
